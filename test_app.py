21fb5bf8194cf14837bf941fb16fd14d5f63aa9d
Ubuntu <ubuntu@ip-172-31-34-239.eu-north-1.compute.internal>
2025-07-14 10:25:04 +0000

    fix(test): create test client for add route

--- test_app.py
+++ test_app.py
@@ -2,6 +2,7 @@ from app import app
 
 
 def test_add():
-    response = client.get("/add?a=2&b=3")
-    assert response.status_code == 200
-    assert response.json["result"] == 5
+    with app.test_client() as client:
+        response = client.get("/add?a=2&b=3")
+        assert response.status_code == 200
+        assert response.json["result"] == 5
